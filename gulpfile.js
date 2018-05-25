3de5b307c27917246e4fc7d38fe4148cb58c3da3
Dominik <domi38h@gmail.com>
2018-05-25 15:56:14 +0200

    Mobile Homepage

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,14 @@
 // Dependencies
 // --------------------------------------------
 var autoprefixer = require('autoprefixer'),
+    cssnano = require('cssnano'),
+    postcss = require('gulp-postcss'),
     concat = require('gulp-concat'),
+    del = require('del'),
     gulp = require('gulp'),
     plumber = require('gulp-plumber'),
     sass = require('gulp-sass'),
-    cssnano = require('cssnano'),
-    postcss = require('gulp-postcss'),
+    sourcemaps = require('gulp-sourcemaps'),
     rename = require('gulp-rename'),
     uglify = require('gulp-uglify'),
     images = require('gulp-imagemin'),
@@ -17,14 +19,14 @@ var autoprefixer = require('autoprefixer'),
 // paths
 var styleSrc = 'source/scss/**/*.scss',
     styleDest = 'build/assets/css/',
-    htmlSrc = 'source/**/*.html',
+    htmlSrc = 'source/*.html',
     htmlDest = 'build/',
-    imgSrc = 'source/img/**/*',
-    imgDest = 'build/assets/img/*',
-    vendorSrc = 'source/js/vendors/*.js',
+    vendorSrc = 'source/js/vendors/',
     vendorDest = 'build/assets/js/',
     scriptSrc = 'source/js/*.js',
-    scriptDest = 'build/assets/js/';
+    scriptDest = 'build/assets/js/',
+    imgSrc = 'source/img/**/*',
+    imgDest = 'build/assets/img/*';
 
 
 
@@ -32,30 +34,28 @@ var styleSrc = 'source/scss/**/*.scss',
 // Stand Alone Tasks
 // --------------------------------------------
 
-// Move html 
+
+// Copy HTML
 gulp.task('html', function() {
-  gulp.src('source/**/*.html')
+    return gulp.src('source/**/*.html')
         .pipe(plumber())
-        .pipe(gulp.dest('build/'));
+        .pipe(gulp.dest('build'));
 });
 
-// Compile sass files 
+
+// Compiles all SCSS files
 gulp.task('sass', function() {
-  gulp.src('source/scss/**/*.scss')
+    gulp.src('source/scss/**/*.scss')
         .pipe(plumber())
         .pipe(sass({
-          outputStyle: 'expanded'
+            outputStyle: 'expanded'
         }))
+        .pipe(postcss([autoprefixer()]))
         .pipe(rename({
-          basename: 'style'
+            basename: 'style'
         }))
-        .pipe(postcss([
-          autoprefixer()
-        ]))
         .pipe(gulp.dest('build/assets/css'))
-        .pipe(postcss([
-          cssnano()
-        ]))
+        .pipe(postcss([cssnano()]))
         .pipe(rename({
             basename: 'style',
             suffix: '.min'
@@ -63,10 +63,9 @@ gulp.task('sass', function() {
         .pipe(gulp.dest('build/assets/css'));
 });
 
-
-// Minify imgs
+// Images
 gulp.task('images', function() {
-  gulp.src('source/img/**/*')
+    gulp.src('source/img/*')
         .pipe(plumber())
         .pipe(images())
         .pipe(gulp.dest('build/assets/img'));
@@ -76,6 +75,7 @@ gulp.task('images', function() {
 gulp.task('scripts', function() {
     gulp.src('source/js/*.js')
         .pipe(plumber())
+        .pipe(concat('script.js'))
         .pipe(uglify())
         .pipe(gulp.dest('build/assets/js'));
 });
@@ -87,7 +87,6 @@ gulp.task('vendors', function() {
                 'source/js/vendors/jquery.min.js',
                 'source/js/vendors/*.js'
             ])
-        .pipe(plumber())
         .pipe(concat('vendors.js'))
         .pipe(uglify())
         .pipe(gulp.dest('build/assets/js'));
@@ -106,15 +105,15 @@ gulp.task('watch', function(){
         notify: false
     });
 
-    gulp.watch(styleSrc,['sass']);
     gulp.watch(htmlSrc,['html']);
+    gulp.watch(imgSrc,['images']);
+    gulp.watch(styleSrc,['sass']);
     gulp.watch(scriptSrc,['scripts']);
     gulp.watch(vendorSrc,['vendors']);
-    gulp.watch(imgSrc,['images']);
-    gulp.watch(['build/*.html', 'build/assets/css/*.css', 'build/assets/js/*.js', 'build/assets/img/*']).on('change', browserSync.reload);
+    gulp.watch(['build/*.html', 'build/assets/css/*.css', 'build/assets/js/*.js', 'build/assets/js/vendors/*.js']).on('change', browserSync.reload);
 
 });
 
 
 // use default task to launch Browsersync and watch JS files
-gulp.task('default', ['html', 'sass', 'images', 'scripts', 'vendors', 'watch'], function () {});
+gulp.task('default', [ 'html', 'images', 'sass', 'scripts', 'vendors', 'watch'], function () {});
