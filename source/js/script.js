7127f8d04a8c3a8c5de38e507ecf25788fe57556
Dominik <domi38h@gmail.com>
2018-05-26 12:29:53 +0200

    Desktop Homepage

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,61 @@
 $(document).ready(function() {
 
+  // Smooth Scroll
+  $('a[href*="#"]')
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+      if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+        && 
+        location.hostname == this.hostname
+      ) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          event.preventDefault();
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000, function() {
+            // var $target = $(target);
+            // $target.focus();
+            // if ($target.is(":focus")) {
+            //   return false;
+            // } else {
+            //   $target.attr('tabindex','-1');
+            //   $target.focus();
+            // };
+          });
+        }
+      }
+    });
+
+  // Check if header is in viewport
+  $.fn.isInViewport = function() {
+    var elementTop = $(this).offset().top;
+    var elementBottom = elementTop + $(this).outerHeight();
+  
+    var viewportTop = $(window).scrollTop();
+    var viewportBottom = viewportTop + $(window).height();
+  
+    return elementBottom > viewportTop && elementTop < viewportBottom;
+  };
+  
+  $(window).on('resize scroll', function() {
+    var sdBtn = $('.main-nav__arrow');
+    if($('#top').isInViewport()) {
+      if(sdBtn.hasClass('turn')) {
+        sdBtn.removeClass('turn');
+        sdBtn.parent().attr('href', '#content');
+      }
+    } else {
+      if(!sdBtn.hasClass('turn')) {
+        sdBtn.addClass('turn');
+        sdBtn.parent().attr('href', '#top');
+      }
+    }
+  });
+
   // Slider Reviews
   $('.reviews__slider').slick({
     centerMode: true,
